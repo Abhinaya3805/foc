55a070f005466b57286c1ed50e226ddaa515d6af
Abhinaya3805 <129161735+Abhinaya3805@users.noreply.github.com>
2023-08-10 09:05:05 +0530

    Add files via upload

---
+++ positive or negative.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter a number");
+	scanf("%d",&num);
+	if(num==0)
+	printf("num is positive");
+	else(num>0);
+	printf("num is negative");
+	return 0;
+}
